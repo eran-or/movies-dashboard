aa8bdf59d23786e530263193bd30ee2cad859522
eranor <e.or1976@gmail.com>
2018-12-27 16:16:50 +0200

    responsive mobile and sm

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -20,18 +20,18 @@ class Home extends Component {
   render() {
     const { movies } = this.state
     return (
-      <div className="App">
+      <div className="App row flex-column flex-sm-row m-auto">
         {movies && movies.map((m, i) =>
-          <div key={i} className="movie-box d-flex align-items-center rounded border flex-column mx-auto mb-2">
+          <div key={i} className="movie-box d-flex align-items-center align-items-sm-start rounded border flex-column mx-auto mb-2 flex-sm-row">
             <img src={m.Poster} alt="" className="rounded scaleDown" />
-            <div className="col-12 py-2">
+            <div className="col-12 py-2 movie-info col-sm-7">
               <h5>{m.Title}</h5>
               <p className="m-0"><strong>Year:</strong>{m.Year}</p>
               <p className="m-0"><strong>Runtime:</strong>{m.Runtime}</p>
               <p className="m-0"><strong>Genre:</strong>{m.Genre}</p>
               <p className="m-0"><strong>Director:</strong>{m.Director}</p>
             </div>
-            <button type="button" className="w-100 btn btn-primary">Edit</button>
+            <button type="button" className="w-100 btn btn-primary align-self-sm-end">Edit</button>
           </div>
         )}
 
