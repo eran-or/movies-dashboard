e18024618600dda0cd9833f9eeec8588434f6056
eranor <e.or1976@gmail.com>
2019-01-08 17:37:48 +0200

    Updates

--- precache-manifest.980014682804355967642d2836985469.js
+++ precache-manifest.980014682804355967642d2836985469.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/movies-dashboard/static/js/runtime~main.132fc9df.js"
   },
   {
-    "revision": "0191ea176750d836e477",
-    "url": "/movies-dashboard/static/js/main.0191ea17.chunk.js"
+    "revision": "5f859d0cec4b542af675",
+    "url": "/movies-dashboard/static/js/main.5f859d0c.chunk.js"
   },
   {
     "revision": "22cecc37ff9a93236d4a",
     "url": "/movies-dashboard/static/js/1.22cecc37.chunk.js"
   },
   {
-    "revision": "0191ea176750d836e477",
+    "revision": "5f859d0cec4b542af675",
     "url": "/movies-dashboard/static/css/main.7ec0b595.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/movies-dashboard/static/css/1.53d39138.chunk.css"
   },
   {
-    "revision": "e15fabbf8982660ebe547a31ba9b4db2",
+    "revision": "11a20f512a010e3d1e8f04ab6acd3127",
     "url": "/movies-dashboard/index.html"
   }
 ];
