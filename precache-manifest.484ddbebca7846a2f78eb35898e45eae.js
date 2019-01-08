1cc82bd9172e5774a79b4569146f2b15bf0399cd
eranor <e.or1976@gmail.com>
2019-01-08 17:26:22 +0200

    Updates

---
+++ precache-manifest.484ddbebca7846a2f78eb35898e45eae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "132fc9dffd013dfc210f",
+    "url": "/movies-dashboard/static/js/runtime~main.132fc9df.js"
+  },
+  {
+    "revision": "b0e94b453713255ed208",
+    "url": "/movies-dashboard/static/js/main.b0e94b45.chunk.js"
+  },
+  {
+    "revision": "22cecc37ff9a93236d4a",
+    "url": "/movies-dashboard/static/js/1.22cecc37.chunk.js"
+  },
+  {
+    "revision": "b0e94b453713255ed208",
+    "url": "/movies-dashboard/static/css/main.7ec0b595.chunk.css"
+  },
+  {
+    "revision": "22cecc37ff9a93236d4a",
+    "url": "/movies-dashboard/static/css/1.53d39138.chunk.css"
+  },
+  {
+    "revision": "1afaebbed25ad6b7180a4f9a15855e78",
+    "url": "/movies-dashboard/index.html"
+  }
+];
